ded5d5ce4333aaa38152e9de86d5d1c809e208fb
hwang-code14 <bbss67117@gmail.com>
2022-12-16 18:47:45 +0900

    vue 기초강의

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,26 +1,88 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div class="black-bg" v-if="modarBoolean==true">
+    <div class="white-bg">
+        <h4>상세페이지</h4>
+        <p>상세페이지 내용</p>
+    </div>
+  </div>
+
+  <div class="menu">
+    <a v-for="topNavigator in topNavigator" :key="topNavigator"> {{topNavigator}} </a>
+  </div>
+    
+  <div v-for="(oneroomlist) in oneroomlist" :key="oneroomlist">
+      <img :src="oneroomlist.image" class="room-img">
+      <h4>이름:{{oneroomlist.title}}</h4>
+      <p>가격:{{oneroomlist.content}}</p>
+      <p>가격:{{oneroomlist.price}}</p>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import data from './data.js'; 
+
 
 export default {
   name: 'App',
+  data() {
+    return {
+      oneroomlist : data,
+      modarBoolean : false,
+      declarationCount : [0,0,0],
+      topNavigator : ['Home', 'Shop', 'About'],
+      products: ['역삼동원룸', '천호동원룸', '마포구원룸']
+    }
+  },
+  methods : {
+    increase(){
+      this.declarationCount ++
+    }
+  },
+
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
+
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0, 0, 0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
+ .menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+ }
+ .menu a {
+  color: white;
+  padding: 10px;
+ }
 </style>
